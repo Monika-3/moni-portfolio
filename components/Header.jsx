d70774c7afbfc92f2ed39361a595360f4a3fece8
Monicka3 <monimska318@gmail.com>
2025-09-11 23:46:19 +0530

    feat :  page transition animation

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,8 @@ import { Button } from "./ui/button";
 
 //components
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+
 const Header = () =>{
     return(
         <header className="py-8 xl:py-12 text-white">
@@ -25,7 +27,7 @@ const Header = () =>{
 
                 {/*mobile nav*/}
                 <div className="xl:hidden">
-                    Mobile Nav
+                    <MobileNav/>
                 </div>
                 
             </div>
